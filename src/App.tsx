d410688afd2875d55259f181d5849fb0bdcf9199
Karol Janas <karol.x.janas@gsk.com>
2025-10-06 23:24:46 +0200

    show users on ui

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
       console.log(response?.data)
       setUsers(response?.data)
     }catch(error)  {
+      console.error(error)
       if (axios.isAxiosError(error)) {
         setError(error.message)
       } else if (error instanceof Error) {
@@ -27,22 +28,24 @@ function App() {
   }
 
 
+
   useEffect(() => {
     getData()
   }, [])
   
   return (
     <>
-      <div className="text-red-500">
-        <h1 className="font-bolder text-green flex justify-center items-center underline">Users List</h1>
-        <div>
-          { users && users?.map(user) => (
-            <div key={users.id}>
-              <p>User<span>{users?.username}</span></p>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800 text-gray800 p-6">
+        <h1 className="text-4xl font-bold text-gray-200 mb-6">Users List</h1>
+        <div className="w-full max-w-5xl bg-gray-700 shadow-lg rounded-2xl p-4 grid grid-cols-2 gap-2">
+          { users && users?.map(user => (
+            <div key={user.id}>
+              <p className='text-lg font-semibold  text-white '>User: <span className='text-green-500 hover:bg-gray-800 cursor-pointer'>{user?.username}</span></p>
             </div>
-          )}
+          )
+        )}
 
-          {error && <div className="text-red-500 font-bold border bg-amber-600">{error.messae}</div>}
+          {error && <div className="text-red-500 text-center ">{error}</div>}
         </div>
       </div>
       
