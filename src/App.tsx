1876a1c5f42676a3857a2ece4695d1a55caaf7a2
EliteBook <kontoserfis@gmail.com>
2025-10-06 21:42:48 +0200

    add users fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,29 @@ function App() {
 
 
   const [ users, setUsers ] = useState<any[]>([])
+  const [ error, setError ] = useState<string | null>(null)
+
+
+  const getData = async() => {
+    try {
+      const response = await axios.get('http://jsonplaceholder.typicode.com/users')
+      console.log(response?.data)
+      setUsers(response?.data)
+    }catch(error)  {
+      if (axios.isAxiosError(error)) {
+        setError(error.message)
+      } else if (error instanceof Error) {
+        setError(error.message)
+      } else {
+        setError('Uknown error')
+      }
+    }
+  }
+
+
+  useEffect(() => {
+    getData()
+  }, [])
   
   return (
     <>
